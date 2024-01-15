1475ab7fd7f3960c91b720a1cbd452ca49c9a3b3
Rafael Santos <rafa.05125@gmail.com>
2024-01-15 20:30:26 -0300

    Admin, LoginDTO

---
+++ Minimal-Vehicle-API/Domain/Entities/LoginDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Minimal_Vehicle_API.Domain.Entities
+{
+    public class LoginDTO
+    {
+        public string Email { get; set; } = default!;
+
+        public string Password { get; set; } = default!;
+    }
+
+}
