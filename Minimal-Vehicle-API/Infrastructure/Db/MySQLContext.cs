b5c7a533f05c2c233535620aadd58cac13f0542c
Rafael Santos <rafa.05125@gmail.com>
2024-01-15 20:31:10 -0300

    MySQLContext, first migration

---
+++ Minimal-Vehicle-API/Infrastructure/Db/MySQLContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Minimal_Vehicle_API.Domain.Entities;
+
+namespace Minimal_Vehicle_API.Infrastructure.Db
+{
+    public class MySQLContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+        public MySQLContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public DbSet<Admin> Admins { get; set; } = default!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var stringConexao = _configuration.GetConnectionString("mysql")?.ToString();
+                if (!string.IsNullOrEmpty(stringConexao))
+                {
+                    optionsBuilder.UseMySql(
+                        stringConexao,
+                        ServerVersion.AutoDetect(stringConexao)
+                    );
+                }
+            }
+        }
+    }
+}
