8a9ad6397348b6060a12c65afc60f04915db8d1a
Rafael Santos <rafa.05125@gmail.com>
2024-01-16 09:45:10 -0300

    AdminService

---
+++ Minimal-Vehicle-API/Services/AdminService.cs
@@ -0,0 +1,26 @@
+﻿using Minimal_Vehicle_API.Domain.Entities;
+using Minimal_Vehicle_API.Domain.Interfaces;
+using Minimal_Vehicle_API.Infrastructure.Db;
+
+namespace Minimal_Vehicle_API.Services
+{
+    public class AdminService : IAdminService
+    {
+        private readonly MySQLContext _context;
+
+        public AdminService(MySQLContext context)
+        {
+            _context = context;
+        }
+
+        public Admin? Login(LoginDTO loginDTO)
+        {
+            var adm = _context.Admins.Where
+                (a => a.Email == loginDTO.Email && 
+                a.Password == loginDTO.Password)
+                .FirstOrDefault();
+       
+            return adm; 
+        }
+    }
+}
