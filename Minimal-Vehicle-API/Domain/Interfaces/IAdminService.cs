8247e49e6b43ab1b118b02558d56184b79a90e20
Rafael Santos <rafa.05125@gmail.com>
2024-01-16 09:44:56 -0300

    IAdminService

---
+++ Minimal-Vehicle-API/Domain/Interfaces/IAdminService.cs
@@ -0,0 +1,9 @@
+﻿using Minimal_Vehicle_API.Domain.Entities;
+
+namespace Minimal_Vehicle_API.Domain.Interfaces
+{
+    public interface IAdminService
+    {
+        Admin? Login(LoginDTO loginDTO);
+    }
+}
