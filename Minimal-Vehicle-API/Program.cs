542eb7409056e49582bef9baf4d0e3c1b580337c
Rafael Santos <rafa.05125@gmail.com>
2024-01-16 09:46:17 -0300

    Adm login validation

--- Minimal-Vehicle-API/Program.cs
+++ Minimal-Vehicle-API/Program.cs
@@ -1,10 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Minimal_Vehicle_API.Domain.Entities;
+using Minimal_Vehicle_API.Domain.Interfaces;
 using Minimal_Vehicle_API.Infrastructure.Db;
+using Minimal_Vehicle_API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.   
+
+builder.Services.AddScoped<IAdminService, AdminService>();
 
 builder.Services.AddDbContext<MySQLContext>(options =>
 {
@@ -19,9 +24,9 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World");
 
-app.MapPost("/login", (LoginDTO loginDTO) =>
+app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdminService adminService) =>
 {
-    if (loginDTO.Email == "adm@teste.com" && loginDTO.Password == "123456")
+    if (adminService.Login(loginDTO) != null)
         return Results.Ok("Logged");
     else
         return Results.Unauthorized();
