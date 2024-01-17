8b6eb56b07e0d1737ca2d0547c0f8a96545cf26a
Rafael Santos <rafa.05125@gmail.com>
2024-01-17 19:01:54 -0300

    IVehicleService, VehicleService

---
+++ Minimal-Vehicle-API/Domain/Interfaces/IVehicleService.cs
@@ -0,0 +1,15 @@
+﻿using Minimal_Vehicle_API.Domain.Entities;
+
+namespace Minimal_Vehicle_API.Domain.Interfaces
+{
+    public interface IVehiceService
+    {
+        List<Vehicle> GetVehicles(int page = 1, string? name = null, string? brand = null);    
+        Vehicle? FindById(int id);
+        void Add(Vehicle vehicle);
+
+        void Update(Vehicle vehicle);
+
+        void Delete(Vehicle vehicle);
+    }
+}
---
+++ Minimal-Vehicle-API/Services/VehicleService.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Minimal_Vehicle_API.Domain.Entities;
+using Minimal_Vehicle_API.Domain.Interfaces;
+using Minimal_Vehicle_API.Infrastructure.Db;
+
+namespace Minimal_Vehicle_API.Services
+{
+    public class VehicleService : IVehiceService
+    {
+        private readonly MySQLContext _context;
+
+        public VehicleService(MySQLContext context)
+        {
+            _context = context;
+        }
+
+        public List<Vehicle> GetVehicles(int page = 1, string? name = null, string? brand = null)
+        {
+            var query = _context.Vehicles.AsQueryable();
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                query = query.Where
+                    (v => EF.Functions.Like(v.Name.ToLower(), $"%{name}%"));
+            }
+
+            int itensPerPage = 10;
+
+            query = query.Skip((page - 1) * itensPerPage).Take(itensPerPage);
+
+            return [.. query];
+        }
+
+        public Vehicle? FindById(int id)
+        {
+            return _context.Vehicles.Where(v => v.Id == id).FirstOrDefault();
+        }
+
+        public void Add(Vehicle vehicle)
+        {
+            _context.Vehicles.Add(vehicle);
+            _context.SaveChanges();
+        }
+
+        public void Update(Vehicle vehicle)
+        {
+            _context.Vehicles.Update(vehicle);
+            _context.SaveChanges();
+        }
+
+        public void Delete(Vehicle vehicle)
+        {
+            _context.Remove(vehicle);
+            _context.SaveChanges();
+        }
+    }
+}
