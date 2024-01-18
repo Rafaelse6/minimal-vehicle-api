32356a128a40fee100dae423d392c16db941c660
Rafael Santos <rafa.05125@gmail.com>
2024-01-18 00:25:06 -0300

    VehicleDTO

---
+++ Minimal-Vehicle-API/Domain/DTOs/VehicleDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Minimal_Vehicle_API.Domain.DTOs
+{
+    public record VehicleDTO
+    {
+        public string Name { get; set; } = default!;
+
+        public string Brand { get; set; } = default!;
+
+        public int Year { get; set; } = default!;
+    }
+}
