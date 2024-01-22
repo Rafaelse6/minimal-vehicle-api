a414bdef45b6fd57093e4ed7b40e630140be3307
Rafael Santos <rafa.05125@gmail.com>
2024-01-22 09:55:13 -0300

    AdminModelView

---
+++ Minimal-Vehicle-API/Domain/ModelViews/AdminModelView.cs
@@ -0,0 +1,13 @@
+﻿using Minimal_Vehicle_API.Domain.Enums;
+
+namespace Minimal_Vehicle_API.Domain.ModelViews
+{
+    public record AdminModelView
+    {
+        public int Id { get; set; } = default!;
+
+        public string Email { get; set; } = default!;
+
+        public string Profile { get; set; } = default!;
+    }
+}
