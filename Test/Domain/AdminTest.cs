4a3c03b47fd8760d31e69f83c0076983dc48a1ef
Rafael Santos <rafa.05125@gmail.com>
2024-01-25 20:16:43 -0300

    Test project, Admin test

---
+++ Test/Domain/AdminTest.cs
@@ -0,0 +1,28 @@
+﻿using Minimal_Vehicle_API.Domain.Entities;
+
+namespace Test.Domain
+{
+    [TestClass]
+    public class AdminTest
+    {
+
+        [TestMethod]
+        public void GetSetPropertiesTest()
+        {
+            //Arrangee
+            var adm = new Admin();
+
+            //Act
+            adm.Id = 1;
+            adm.Email = "test@test.com";
+            adm.Password = "password";
+            adm.Profile = "Adm";
+
+            //Assert
+            Assert.AreEqual(1, adm.Id);
+            Assert.AreEqual("test@test.com", adm.Email);
+            Assert.AreEqual("password", adm.Password);
+            Assert.AreEqual("Adm", adm.Profile);
+        }
+    }
+}
