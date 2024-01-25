fbc7a4dcdcb63869f1f4ec49117ba83fb9d25926
Rafael Santos <rafa.05125@gmail.com>
2024-01-25 20:22:08 -0300

    VehicleTest

---
+++ Test/Domain/VehicleTest.cs
@@ -0,0 +1,28 @@
+﻿using Minimal_Vehicle_API.Domain.Entities;
+
+namespace Test.Domain
+{
+    [TestClass]
+    public class VehicleTest
+    {
+        [TestMethod]
+        public void GetSetPropertiesTest()
+        {
+            // Arrange
+            var vehicle = new Vehicle();
+
+            // Act
+            vehicle.Id = 1;
+            vehicle.Name = "Test";
+            vehicle.Brand = "Ford";
+            vehicle.Year = 2024;
+
+            // Arrange
+            Assert.AreEqual(vehicle.Id, 1);
+            Assert.AreEqual(vehicle.Name, "Test");
+            Assert.AreEqual(vehicle.Brand, "Ford");
+            Assert.AreEqual(vehicle.Year, 2024);
+
+        }
+    }
+}
